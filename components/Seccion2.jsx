839b22e9d263ef36dbe5e0774169963eecd67b92
PabloAngulo12 <pc.angulo23@info.uas.edu.mx>
2025-01-13 11:08:55 -0700

    Update Seccion2.jsx

--- components/Seccion2.jsx
+++ components/Seccion2.jsx
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
-import imagen2 from '../assets/HaloODST.jpg'; // Importa la imagen
+import React from 'react';
+import imagen2 from '../assets/HaloODST.jpg'; 
 
 function Seccion2() {
   console.log('Seccion2 is rendering');
-  const [textareaValue, setTextareaValue] = useState(''); // Estado para el área de texto
-
-  const handleTextareaChange = (e) => {
-    setTextareaValue(e.target.value);
-  };
 
   return (
     <section className="App-seccion p-4 flex flex-col items-center">
@@ -20,12 +15,10 @@ function Seccion2() {
             cada vez más difíciles para ganar puntos y sobrevivir el mayor tiempo posible. Nuevas copias del juego también contenían claves de acceso a 
             la beta multijugador de Halo: Reach, que se activó el 3 de mayo de 2010.</p>
         <img src={imagen2} alt="Imagen en Sección 2" className="mt-4 rounded shadow-lg" />
-        <div className="mt-4">
-          <textarea value={textareaValue} onChange={handleTextareaChange} className="w-full p-2 rounded border"></textarea>
-        </div>
       </div>
     </section>
   );
 }
 
 export default Seccion2;
+
